ddc222728e9b85bc083e5a0590f3107a543da409
Dan Vega <danvega@gmail.com>
2022-10-06 16:46:34 -0400

    renaming main application class

--- src/main/java/dev/danvega/blogjdbc/Application.java
+++ src/main/java/dev/danvega/blogjdbc/Application.java
@@ -11,13 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 
-import java.time.LocalDateTime;
-
 @SpringBootApplication
-public class BlogJdbcApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogJdbcApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
@@ -26,8 +24,8 @@ public class BlogJdbcApplication {
 			AggregateReference<Author,Integer> author = AggregateReference.to(authorRepository.save(new Author(null, "Dan", "Vega", "danvega@gmail.com", "dvega")).id());
 
 			Post post = new Post( "Dan's First Post", "This is Dan's First Post",author);
-			post.addComment(new Comment( "Dan", "This is a comment", LocalDateTime.now(), LocalDateTime.now()));
-			post.addComment(new Comment( "John", "This is another comment", LocalDateTime.now(), LocalDateTime.now()));
+			post.addComment(new Comment( "Dan", "This is a comment"));
+			post.addComment(new Comment( "John", "This is another comment"));
 			postRepository.save(post);
 		};
 	}
